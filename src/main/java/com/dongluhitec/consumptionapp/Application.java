cd5ef26ed20649d2b464bfcef307fe3425b28db0
LAPTOP-1U9MTLUV\panmingzhi <154341736@qq.com>
2018-05-23 15:36:54 +0800

    修改成简单的依赖版本

--- src/main/java/com/dongluhitec/consumptionapp/Application.java
+++ src/main/java/com/dongluhitec/consumptionapp/Application.java
@@ -1,10 +1,7 @@
 package com.dongluhitec.consumptionapp;
 
-import com.dongluhitec.consumptionapp.service.OnecardService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,13 +10,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public FilterRegistrationBean testFilterRegistration() {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(new OnecardService());//添加过滤器
-		registration.addUrlPatterns("/*");//设置过滤路径，/*所有路径
-		registration.setName("OnecardService");//设置优先级
-		registration.setOrder(1);//设置优先级
-		return registration;
-	}
 }
